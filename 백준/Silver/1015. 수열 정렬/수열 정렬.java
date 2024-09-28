477db4ea05676642908e616881d0d75946ee3582
Wanho <115472002+kimwanho23@users.noreply.github.com>
2024-09-28 15:02:15 +0900

    [Silver IV] Title: 수열 정렬, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.java
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -6,35 +6,51 @@ import java.util.*;
 public class Main {
 
     static int[] A;
-    static int[] B;
     static int[] P;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
         A = new int[N];
-        B = new int[N];
         P = new int[N];
+
+        // A 배열의 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-
             A[i] = Integer.parseInt(st.nextToken());
-            B[i] = A[i];
-            P[i] = 0;
         }
-        Arrays.sort(B);
 
+        // A 배열의 값과 그 인덱스를 저장하는 Pair 리스트 생성
+        List<Pair> pairs = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (B[j] == A[i]){
-                    P[i] = j;
-                    B[j] = -1;
-                    break;
-                }
-            }
+            pairs.add(new Pair(A[i], i));
         }
+
+        // 값을 기준으로 정렬
+        pairs.sort(Comparator.comparingInt(p -> p.value));
+
+        // 정렬된 순서에 따라 P 배열의 값을 설정
         for (int i = 0; i < N; i++) {
-            System.out.print(P[i] + " ");
+            P[pairs.get(i).index] = i;
+        }
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(P[i]).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    // 값을 저장할 Pair 클래스 (값과 원래 인덱스)
+    static class Pair {
+        int value;
+        int index;
+
+        Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
     }
 }
