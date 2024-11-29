89623f3d0de3765ea64aa42a363d878021a09207
Wanho <115472002+kimwanho23@users.noreply.github.com>
2024-11-29 18:27:33 +0900

    [Gold V] Title: Contact, Time: 164 ms, Memory: 17080 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        String s = "(100+1+|01)+";
+
+        for (int i = 0; i < T; i++) {
+            String sp = br.readLine();
+            if (sp.matches(s)) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
